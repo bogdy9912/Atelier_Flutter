d5035d59e284022dcca49e2978847560c2e49074
Bogdan M <bogdancons99@gmail.com>
2020-11-24 13:41:03 +0200

    .

--- guess_my_number/lib/main.dart
+++ guess_my_number/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -24,16 +24,27 @@ class Homapage extends StatefulWidget {
 class _HomapageState extends State<Homapage> {
   int randomNumber;
   int myNumber;
+  String option;
+  TextEditingController controller = TextEditingController();
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     randomNumber = Random().nextInt(100);
+    option = '';
   }
 
-  void submited(){
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
 
+  void reloadGuessing() {
+    controller.clear();
+    myNumber = null;
+    option = '';
+    randomNumber = Random().nextInt(100);
   }
 
   @override
@@ -43,43 +54,135 @@ class _HomapageState extends State<Homapage> {
         title: const Text('Guess my number'),
         centerTitle: true,
       ),
-      body: Column(
-        children: <Widget>[
-          const Text(
-            'I\'m thinking of a number between 1 and 100.',
-            style: TextStyle(fontSize: 20),
-            textAlign: TextAlign.center,
-          ),
-          const Text(
-            'It\'s your turn to guess my number!',
-            style: TextStyle(fontSize: 18),
-            textAlign: TextAlign.center,
-          ),
-          if (myNumber != null) Text('You tried $myNumber\n Try harder'),
-          Card(
-            child: Column(
-              children: <Widget>[
-                const Text(
-                  'Try a number!',
-                  style: TextStyle(fontSize: 28),
-                ),
-                TextField(
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.digitsOnly
-                  ],
-                  onChanged: (String value) {
-                    myNumber = int.parse(value);
-                  },
-                ),
-                RaisedButton(
-                  onPressed: () {
-                  },
-                  child: const Text('Guess'),
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.all(12),
+              child: const Text(
+                'I\'m thinking of a number between 1 and 100.',
+                style: TextStyle(fontSize: 28),
+                textAlign: TextAlign.center,
+              ),
             ),
-          ),
-        ],
+            Container(
+              margin: const EdgeInsets.all(12),
+              child: const Text(
+                'It\'s your turn to guess my number!',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            if (myNumber != null)
+              Text('You tried $myNumber\n $option',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.grey, fontSize: 35)),
+            Card(
+              margin: const EdgeInsets.all(16),
+              child: Form(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(top: 8, bottom: 12),
+                      child: const Text(
+                        'Try a number!',
+                        style: TextStyle(fontSize: 28),
+                      ),
+                    ),
+                    TextFormField(
+                      controller: controller,
+                      textInputAction: TextInputAction.done,
+                      keyboardType: const TextInputType.numberWithOptions(),
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                      validator: (String value) {
+                        if (value.isEmpty || value == null) {
+                          return 'Please enter a value';
+                        }
+                        if (int.tryParse(value) == null) {
+                          return 'Please enter a number';
+                        } else {
+                          if (int.parse(value) < 1 || int.parse(value) > 100) {
+                            return 'Please enter a value btw 1 and 100';
+                          }
+                        }
+                        return null;
+                      },
+                      onSaved: (String value) {
+                        controller.text = value;
+                      },
+                    ),
+                    Builder(builder: (BuildContext ctx) {
+                      if (option != 'You guessed right') {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(vertical: 12),
+                          child: RaisedButton(
+                            onPressed: () {
+                              Form.of(ctx).validate();
+                              Form.of(ctx).save();
+                              setState(() {
+                                if (int.tryParse(controller.text) >
+                                    randomNumber) {
+                                  option = 'Try lower';
+                                } else {
+                                  if (int.tryParse(controller.text) <
+                                      randomNumber) {
+                                    option = 'Try higher';
+                                  } else {
+                                    option = 'You guessed right';
+                                  }
+                                }
+                                myNumber = int.tryParse(controller.text);
+                                controller.clear();
+                                if (option == 'You guessed right') {
+                                  showDialog<Widget>(
+                                      context: ctx,
+                                      child: AlertDialog(
+                                        title: Text(option),
+                                        content: Text('It was $myNumber'),
+                                        actions: <Widget>[
+                                          FlatButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                              setState(reloadGuessing);
+                                            },
+                                            child: const Text('Try again!'),
+                                          ),
+                                          FlatButton(
+                                            onPressed: () =>
+                                                Navigator.of(context).pop(),
+                                            child: const Text('OK'),
+                                          ),
+                                        ],
+                                      ));
+                                }
+                              });
+                            },
+                            child: const Text('Guess'),
+                          ),
+                        );
+                      } else {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(vertical: 12),
+                          child: RaisedButton(
+                            onPressed: () {
+                              setState(reloadGuessing);
+                            },
+                            child: const Text('Reset'),
+                          ),
+                        );
+                      }
+                    }),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
